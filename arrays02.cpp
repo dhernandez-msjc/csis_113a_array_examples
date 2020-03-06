b88cb091e456bec2c0da3ed383bc10b93bcbc4e4
David C Hernandez <dhernandez@msjc.edu>
2020-03-06 14:55:43 -0800

    added example 02

---
+++ arrays02.cpp
@@ -0,0 +1,17 @@
+// Name: David Hernandez
+// Associate Faculty CSIS MSJC
+// GitHub Link:  https://github.com/dhernandez-msjc/csis_113a_array_examples
+// Example 02 - Practice with a vector
+
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int main() {
+    // declare and initialize an array with pre-filled data
+    vector<int> numbers(5);
+
+
+
+    return 0;
+}
