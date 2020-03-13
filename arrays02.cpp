9f781b17d37b04a860bb23e78a54d8eb5b3d5e85
dhernandez <dhernandez@msjc.edu>
2020-03-13 11:50:51 -0700

    updated several files

--- arrays02.cpp
+++ arrays02.cpp
@@ -8,14 +8,30 @@
 using namespace std;
 
 int main() {
-    // declare and initialize an array
+    // declare and initialize an array using an object array C++11 feature
+    // notice this style also limits the size of the array, however is safer than using c-style array
+    // you still desginate the type, size and name of the array.
     array<int, 5> numbers = {1, 3, 9, 7, 2};
 
     // use the for each loop to display the elements
     for (auto number : numbers) {
         cout << number << " ";
     }
-    cout << endl;
+    cout << endl << endl;
+
+    // you can update the values just as you would a c-style array
+    numbers[2] = 5;
+
+    // you can also access the value in the same way using the bracket notation
+    cout << "The value at index 02: " << numbers[2] << endl << endl;
+
+    // you can utilize the reverse  pointers of the array to print in reverses
+    // notice here that i is set to the reverse beginning, and stops at the reverse end
+    // this allows you to print the contents of the array backwards.
+    for(auto i = numbers.rbegin(); i != numbers.rend(); ++i) {
+        cout << *i << " ";
+    }
+    cout << endl << endl;
 
     return 0;
 }
